085acfbd0b8eabd4fde1eddd7c83e3c759c95ea2
Manuel Reyes <Manueldjsr@gmail.com>
2023-11-14 14:50:54 -0400

    Added database conn

---
+++ Api/db.js
@@ -0,0 +1,17 @@
+const connectDB =  async ()=>{
+
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            //must add in order to not get any error masseges:
+            useUnifiedTopology:true,
+            useNewUrlParser: true,
+        })
+        console.log(`mongo database is connected!!! ${conn.connection.host} `)
+    }catch(error){
+        console.error(`Error: ${error} `)
+        process.exit(1) //passing 1 - will exit the proccess with error
+    }
+
+}
+
+export default connectDB;
