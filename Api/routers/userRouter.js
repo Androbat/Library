8d9699e2710002e46d2a747d4b375c4b5d4f53f8
efs0-cod3 <eflearnng@gmail.com>
2024-01-23 09:35:10 -0400

    nodemon script applied

--- Api/routers/userRouter.js
+++ Api/routers/userRouter.js
@@ -13,7 +13,7 @@ const { login } = require("../controllers/auth");
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.post("/", createUser);
-router.post("/", login);
+router.post("/login", login);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
