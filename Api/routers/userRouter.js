c74a9bb7c4e9f1238d3aee4e941c6799f8a5a7a2
efs0-cod3 <eflearnng@gmail.com>
2024-01-23 09:44:20 -0400

    t

--- Api/routers/userRouter.js
+++ Api/routers/userRouter.js
@@ -1,18 +1,15 @@
 // routes.js
 const express = require("express");
 const router = express.Router();
-const {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-} = require("../controllers/userController");
-const { login } = require("../controllers/auth");
+const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { authorizeRequest } = require("../middleware/auth.mid")
 
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
 
 module.exports = router;
+
