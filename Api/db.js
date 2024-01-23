c8222a00e15297f3e3a38a89d72fd313ef574233
Manuel Reyes <Manueldjsr@gmail.com>
2024-01-22 21:15:54 -0400

    Fixed merge issue on index

--- Api/db.js
+++ Api/db.js
@@ -1,24 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-<<<<<<< Updated upstream
-const connectDB =  async ()=>{
-
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            //must add in order to not get any error masseges:
-            useUnifiedTopology:true,
-            useNewUrlParser: true,
-        })
-        console.log(`mongo database is connected!!! ${conn.connection.host}`)
-    }catch(error){
-        console.error(`Error: ${error} `)
-        process.exit(1) //passing 1 - will exit the proccess with error
-    }
-
-}
-
-module.exports = connectDB;
-=======
 
 const connectDB = async () => {
     try {
@@ -35,5 +16,4 @@ const connectDB = async () => {
 
 connectDB();
 
->>>>>>> Stashed changes
 
--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
-<<<<<<< Updated upstream
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 const connectDB = require("./db");
 const morgan = require("morgan");
 const { authorizeRequest } = require("./middleware/auth.mid");
@@ -34,29 +34,17 @@ app.post('/signin', createUser);
 
 connectDB();
 
-app.listen(port, () => {
-	console.log(`Success! Your application is running on port ${port}.`);
-=======
-require('dotenv').config();
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.json({"message": "Hello Crud Node Express"});
->>>>>>> Stashed changes
+
 });
 
 
 
-<<<<<<< Updated upstream
-=======
 app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
->>>>>>> Stashed changes
